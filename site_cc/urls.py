ea0bbbfe542f983980203aadce0e931c8da62a62
João Miccuci <joaomicucci3@gmail.com>
2024-09-19 15:59:49 -0300

    login e logout

--- site_cc/urls.py
+++ site_cc/urls.py
@@ -6,5 +6,7 @@ from .import views
 
 urlpatterns = [
     path('',views.pagina_principal, name='pagina_principal'),
+    path('login/',views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
    
 ]
--- site_cc/views.py
+++ site_cc/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def pagina_principal(request):
     return render(request,'pagina_principal.html',{})
+def login_user(request):
+    if request.method =="POST":
+        username= request.POST['username']
+        password= request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("voce esta logado"))
+            return redirect('pagina_principal')
+
+
+        else:   
+            messages.success(request, ("ocorreu um erro tente novamente"))
+            return redirect('login') 
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("voce deslogou"))
+    return redirect('pagina_principal')
+
