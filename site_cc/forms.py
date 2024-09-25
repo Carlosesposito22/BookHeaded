0a1d48d4f5932e0da97d32309ee8d8861cf1b577
João Miccuci <joaomicucci3@gmail.com>
2024-09-25 08:46:12 -0300

    validacao do add clube e ajuste tbm no update club

--- site_cc/forms.py
+++ site_cc/forms.py
@@ -19,7 +19,7 @@ class ClubeForm(forms.ModelForm):
             'modalidade': forms.Select(attrs={'class': 'form-control'}),
             'categoria': forms.Select(attrs={'class': 'form-control'}),
             'descricao': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Descreva em poucas palavras seu clube'}),
-            'sobre': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Conte mais sobre o clube'}),
+            'sobre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Conte mais sobre o clube'}),
             'privado': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
 
@@ -38,7 +38,7 @@ class ClubeEditForm(forms.ModelForm):
             'modalidade': forms.Select(attrs={'class': 'form-control'}),
             'categoria': forms.Select(attrs={'class': 'form-control'}),
             'descricao': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Descreva em poucas palavras seu clube'}),
-            'sobre': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Conte mais sobre o clube'}),
+            'sobre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Conte mais sobre o clube'}),
             'privado': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
 
