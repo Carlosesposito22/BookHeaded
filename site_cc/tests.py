22541eb5ed3f1c8b931e7b17fa740d7925b2aa79
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-10 00:19:06 -0300

    agora certo

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -4600,7 +4600,7 @@ class CriarEnqueteTest(LiveServerTestCase):
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
-        #chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--window-size=1920,1080")
         cls.driver = webdriver.Chrome(options=chrome_options)
 
     @classmethod
